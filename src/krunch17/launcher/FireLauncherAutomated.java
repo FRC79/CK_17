b6d6827e0430fd5b300725b7b7efb0006be98c57
sebLopezCot <sclopez2@hotmail.com>
2014-04-09 22:46:43 -0400

    Revert "Added AutonSettings to FireLauncherAutomated"
    This reverts commit 72d98a8a38b90345a910299e594384df88ad3d12.

--- src/krunch17/launcher/FireLauncherAutomated.java
+++ src/krunch17/launcher/FireLauncherAutomated.java
@@ -6,7 +6,6 @@
 package krunch17.launcher;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
-import krunch17.autonomous.AutonSettings;
 import krunch17.intake.ExtendIntake;
 import krunch17.intake.RollIn;
 import krunch17.intake.StopRoller;
@@ -21,7 +20,7 @@ public class FireLauncherAutomated extends CommandGroup {
     public FireLauncherAutomated() {
         addSequential(new Wait(0.004));
         addSequential(new ExtendIntake());
-        addSequential(new Wait(AutonSettings.EXTEND_INTAKE_DELAY + AutonSettings.FIRE_DELAY));
+        addSequential(new Wait(0.5 + 0.5));
         addSequential(new FireLauncher()); // Launcher will retract after fire
     }
 }
